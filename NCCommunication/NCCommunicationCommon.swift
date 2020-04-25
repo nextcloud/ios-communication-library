005ca4769f1390ef5f5dd331ba44c326818d9c33
marinofaggiana <marino@marinofaggiana.com>
2020-04-25 11:48:47 +0200

    bug fix
    Signed-off-by: marinofaggiana <marino@marinofaggiana.com>

--- NCCommunication/NCCommunicationCommon.swift
+++ NCCommunication/NCCommunicationCommon.swift
@@ -57,6 +57,9 @@ import Alamofire
     @objc let sessionIdentifierBackgroundwifi: String = "com.nextcloud.session.backgroundwifi"
     @objc let sessionIdentifierExtension: String = "com.nextcloud.session.extension"
     
+    // Constant
+    let k_encodeCharacterSet = " #;?@&=$+{}<>,!'*"
+    
     //MARK: - Setup
     
     @objc public func setup(user: String, userId: String, password: String, url: String, userAgent: String?, capabilitiesGroup: String?, nextcloudVersion: Int, delegate: NCCommunicationCommonDelegate?) {
@@ -146,7 +149,7 @@ import Alamofire
         
     func encodeStringToUrl(_ string: String) -> URLConvertible? {
         
-        if let escapedString = string.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) {
+        if let escapedString = encodeString(string) {
             return StringToUrl(escapedString)
         }
         return nil
@@ -154,7 +157,11 @@ import Alamofire
     
     func encodeString(_ string: String) -> String? {
         
-        return string.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+        let allowedCharacterSet = (CharacterSet(charactersIn: k_encodeCharacterSet).inverted)
+        let encodeString = string.addingPercentEncoding(withAllowedCharacters: allowedCharacterSet)
+        //let encodeQuery = string.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+        
+        return encodeString
     }
         
     func StringToUrl(_ string: String) -> URLConvertible? {
@@ -167,7 +174,6 @@ import Alamofire
             return nil
         }
     }
-        
     
     func findHeader(_ header: String, allHeaderFields: [AnyHashable : Any]?) -> String? {
        
@@ -176,6 +182,15 @@ import Alamofire
         
         if let headerValue = keyValues.filter({ $0.0 == header.lowercased() }).first {
             return headerValue.1
+        }
+        return nil
+    }
+    
+    func getHostName(urlString: String) -> String? {
+        if let url = URL(string: urlString) {
+            guard let hostName = url.host else { return nil }
+            guard let scheme = url.scheme else { return nil }
+            return scheme + "://" + hostName
         }
         return nil
     }
