a4b4aca3fc7b29d7ac90efb8d3f4d8dcc9c068d7
marinofaggiana <marino@marinofaggiana.com>
2020-05-22 16:01:53 +0200

    improvements
    Signed-off-by: marinofaggiana <marino@marinofaggiana.com>

---
+++ NCCommunication/NCCommunication+E2EE.swift
@@ -0,0 +1,54 @@
+//
+//  NCCommunication+E2EE.swift
+//  NCCommunication
+//
+//  Created by Marino Faggiana on 22/05/2020.
+//  Copyright © 2020 Marino Faggiana. All rights reserved.
+//  Author Marino Faggiana <marino.faggiana@nextcloud.com>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+extension NCCommunication {
+
+    @objc public func markE2EEFolder(fileId: String, customUserAgent: String? = nil, addCustomHeaders: [String:String]? = nil, completionHandler: @escaping (_ account: String, _ errorCode: Int, _ errorDescription: String?) -> Void) {
+                            
+        let account = NCCommunicationCommon.shared.account
+        let endpoint = "ocs/v2.php/apps/end_to_end_encryption/api/v1/encrypted/" + fileId + "?format=json"
+        
+        guard let url = NCCommunicationCommon.shared.createStandardUrl(serverUrl: NCCommunicationCommon.shared.url, endpoint: endpoint) else {
+            completionHandler(account, NSURLErrorUnsupportedURL, NSLocalizedString("_invalid_url_", value: "Invalid server url", comment: ""))
+            return
+        }
+        
+        let method = HTTPMethod(rawValue: "PUT")
+        let headers = NCCommunicationCommon.shared.getStandardHeaders(addCustomHeaders, customUserAgent: customUserAgent)
+        
+        sessionManager.request(url, method: method, parameters:nil, encoding: URLEncoding.default, headers: headers, interceptor: nil).validate(statusCode: 200..<300).responseJSON { (response) in
+            debugPrint(response)
+            switch response.result {
+            case .failure(let error):
+                let error = NCCommunicationError().getError(error: error, httResponse: response.response)
+                completionHandler(account, error.errorCode, error.description)
+            case .success(let json):
+                let json = JSON(json)
+                completionHandler(account, 0, nil)
+            }
+        }
+    }
+}
