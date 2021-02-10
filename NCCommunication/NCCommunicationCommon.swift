d325e1f05882616e20caee090d7377735fe44b38
marinofaggiana <marino@marinofaggiana.com>
2021-02-10 11:07:58 +0100

    added debugPrint in writelog
    Signed-off-by: marinofaggiana <marino@marinofaggiana.com>

--- NCCommunication/NCCommunicationCommon.swift
+++ NCCommunication/NCCommunicationCommon.swift
@@ -437,11 +437,11 @@ import MobileCoreServices
         guard let text = text else { return }
         guard let date = NCCommunicationCommon.shared.convertDate(Date(), format: "yyyy-MM-dd' 'HH:mm:ss") else { return }
         
-        let textToWrite = "\(date) " + text + "\n"
-        debugPrint(textToWrite)
+        debugPrint(text)
 
         if levelLog > 0 {
             
+            let textToWrite = "\(date) " + text + "\n"
             guard let data = textToWrite.data(using: .utf8) else { return }
             if !FileManager.default.fileExists(atPath: filenameLog) {
                 FileManager.default.createFile(atPath: filenameLog, contents: nil, attributes: nil)
