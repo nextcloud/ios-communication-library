17ca90e57568927d6f576c21a496f4a2973fc385
marinofaggiana <marino@marinofaggiana.com>
2021-05-14 10:59:04 +0200

    Improved chunked file
    Signed-off-by: marinofaggiana <marino@marinofaggiana.com>

--- NCCommunication/NCCommunicationCommon.swift
+++ NCCommunication/NCCommunicationCommon.swift
@@ -368,7 +368,9 @@ import MobileCoreServices
             
             let data = try Data(contentsOf: URL(fileURLWithPath: path + "/" + fileName))
             let dataLen = data.count
+            if dataLen == 0 { return nil }
             let chunkSize = ((1024 * 1000) * sizeInMB)
+            if chunkSize == 0 { return nil }
             let fullChunks = Int(dataLen / chunkSize)
             let totalChunks = fullChunks + (dataLen % 1024 != 0 ? 1 : 0)
                 
