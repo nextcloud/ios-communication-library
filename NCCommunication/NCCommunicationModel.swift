9d4baec45bd3ea69991a546369521fe6ddf6a887
marinofaggiana <marino@marinofaggiana.com>
2021-07-05 18:58:36 +0200

    fix
    Signed-off-by: marinofaggiana <marino@marinofaggiana.com>

--- NCCommunication/NCCommunicationModel.swift
+++ NCCommunication/NCCommunicationModel.swift
@@ -397,6 +397,7 @@ class NCDataFileXML: NSObject {
             <has-preview xmlns=\"http://nextcloud.org/ns\"/>
             <mount-type xmlns=\"http://nextcloud.org/ns\"/>
             <rich-workspace xmlns=\"http://nextcloud.org/ns\"/>
+            <checksums xmlns=\"http://nextcloud.org/ns\"/>
         </d:prop>
         <oc:filter-rules>
             <oc:favorite>1</oc:favorite>
