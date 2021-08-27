757daf6214fcbc225e42f884d3fa9a34759c53a8
marinofaggiana <marino@marinofaggiana.com>
2021-08-27 18:49:32 +0200

    fix
    Signed-off-by: marinofaggiana <marino@marinofaggiana.com>

--- NCCommunication/NCCommunication+API.swift
+++ NCCommunication/NCCommunication+API.swift
@@ -294,7 +294,7 @@ extension NCCommunication {
         
         let headers = NCCommunicationCommon.shared.getStandardHeaders(addCustomHeaders, customUserAgent: customUserAgent)
                
-        sessionManager.request(url, method: method, parameters: nil, encoding: URLEncoding.default, headers: headers, interceptor: nil).response { (response) in
+        sessionManager.request(url, method: method, parameters: nil, encoding: URLEncoding.default, headers: headers, interceptor: nil).validate(statusCode: 200..<300).response { (response) in
             debugPrint(response)
             
             switch response.result {
