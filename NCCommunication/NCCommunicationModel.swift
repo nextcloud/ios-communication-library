3d7026937b2b95ee0ff4907b657ffaa662d46870
marinofaggiana <ios@nextcloud.com>
2022-02-21 15:03:57 +0100

    #1787
    Signed-off-by: marinofaggiana <ios@nextcloud.com>

--- NCCommunication/NCCommunicationModel.swift
+++ NCCommunication/NCCommunicationModel.swift
@@ -1034,7 +1034,7 @@ class NCDataFileXML: NSObject {
                 }
             }
 
-            let results = NCCommunicationCommon.shared.getInternalType(fileName: file.fileName, mimeType: file.contentType, directory: file.directory)
+            let results = NCCommunicationCommon.shared.getInternalType(fileName: file.trashbinFileName, mimeType: file.contentType, directory: file.directory)
             
             file.contentType = results.mimeType
             file.classFile = results.classFile
